c4c29d27ca258879764e6e0ba2261bea5495cdbf
Steve Stearns <steve@tomasara.com>
2024-05-14 10:53:35 -0600

    working adc but refactoring is required as details do not match expectations

--- src/bin/adc.rs
+++ src/bin/adc.rs
@@ -4,7 +4,6 @@
 
 use defmt::*;
 use embassy_executor::Spawner;
-// use embassy_stm32::gpio::low_level::Pin;
 use embassy_stm32::adc::Adc;
 use embassy_stm32::peripherals;
 use embassy_stm32::time::Hertz;
@@ -19,6 +18,7 @@ bind_interrupts!(pub struct Irqs {
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     let mut config = Config::default();
+    // default is not sufficient for clocking the adc so we manually configure it
     {
         use embassy_stm32::rcc::*;
         config.rcc.hse = Some(Hertz::mhz(8));
@@ -28,48 +28,61 @@ async fn main(_spawner: Spawner) {
         config.rcc.pclk1 = Some(Hertz::mhz(24));
         config.rcc.pclk2 = Some(Hertz::mhz(48));
         config.rcc.pll48 = true;
-        config.rcc.adc = Some(AdcClockSource::Pll(Adcpres::DIV1));
+        config.rcc.adc = Some(AdcClockSource::Pll(Adcpres::DIV1));  // 48MHz -> 20.83333 ns
         config.rcc.adc34 = None;
     }
-    let mut p = embassy_stm32::init(config);
+    let p = embassy_stm32::init(config);
     info!("Hello World!");
-    // loop {} // spin forever
-    info!("create ADC...");
+
+    debug!("create ADC...");
     let mut adc = Adc::new(p.ADC1, Irqs, &mut Delay);
-    info!("done");
+    adc.sample_time_for_us(3);  // >= 2.2 us per 6.3.22 in STMicrosystems doc DS9118 Rev 14
+    debug!("done");
 
     let mut temperature = adc.enable_temperature();
-    let mut vref = adc.enable_vref(&mut Delay);
+    let mut vrefint = adc.enable_vref(&mut Delay);
 
-    let vref_sample = adc.read(&mut vref).await;
+    fn convert_to_millivolts(sample: u16, vrefint_sample: u16) -> f64 {
+        let vrefint_cal = 0x05f8; // nominal 1.23V ref factory saved reading at 3.3Vdda, manually read from 0x1ffff7ba on my discovery board
+        let vdda_mv = f64::from(adc::VDDA_CALIB_MV) * f64::from(vrefint_cal) / f64::from(vrefint_sample) ;
+        let mv_per_count = vdda_mv / f64::from(adc::ADC_MAX);
 
-    let convert_to_millivolts = |sample| {
-        // From http://www.st.com/resource/en/datasheet/DM00071990.pdf
-        // 6.3.24 Reference voltage
-        const VREFINT_MV: u32 = 1210; // mV
+        f64::from(sample) * mv_per_count
+    }
 
-        (u32::from(sample) * VREFINT_MV / u32::from(vref_sample)) as u16
-    };
+    fn convert_to_celcius(sample: u16, vrefint_sample: u16) -> f64 {
+        // let ts_cal1 = 0x06ca; // 30degC factory saved reading at 3.3Vdda, manually read from 0x1ffff7b8 on my discovery board
+        // let ts_cal2 = 0x0507; // 110degC factory saved reading at 3.3Vdda, manually read from 0x1ffff7c2 on my discovery board
+        // It appears that the factory values are stored backwards *or* the datasheets documents them backwards
+        let ts_cal1 =  0x0507;// (swapped) 30degC factory saved reading at 3.3Vdda, manually read from 0x1ffff7c2 on my discovery board
+        let ts_cal2 = 0x06ca; // (swapped) 110degC factory saved reading at 3.3Vdda, manually read from 0x1ffff7b8 0x1ffff7c2 on my discovery board
+        let vrefint_cal = 0x05f8; // nominal 1.23V ref factory saved reading at 3.3Vdda, manually read from 0x1ffff7ba on my discovery board
+        let slope = (110 - 30) as f64 / (convert_to_millivolts(ts_cal2, vrefint_cal) - convert_to_millivolts(ts_cal1, vrefint_cal)); // degC/mV
+        debug!("slope: {} degC/mv", slope);
+        let intercept_30deg = convert_to_millivolts(ts_cal1, vrefint_cal);
+        debug!("30deg point: {}", intercept_30deg);
 
-    let convert_to_celcius = |sample| {
-        // From http://www.st.com/resource/en/datasheet/DM00071990.pdf
-        // 6.3.22 Temperature sensor characteristics
-        const V25: i32 = 760; // mV
-        const AVG_SLOPE: f32 = 2.5; // mV/C
+        (convert_to_millivolts(sample, vrefint_sample) - intercept_30deg) * slope + 30.0
+    }
 
-        let sample_mv = convert_to_millivolts(sample) as i32;
-
-        (sample_mv - V25) as f32 / AVG_SLOPE + 25.0
-    };
-
-    info!("Vref sample: {}", vref_sample);
-    const MAX_ADC_SAMPLE: u16 = (1 << 12) - 1;
-    info!("VCCA: {} mV", convert_to_millivolts(MAX_ADC_SAMPLE));
+    for _ in 0..10 {
+    let vrefint_cal = 0x05f8; // nominal 1.23V ref factory saved reading at 3.3Vdda, manually read from 0x1ffff7ba on my discovery board
+    let vrefint_sample = adc.read(&mut vrefint).await;
+    let vdda_mv = f64::from(adc::VDDA_CALIB_MV) * f64::from(vrefint_cal) / f64::from(vrefint_sample) ;
+    debug!("vdda_mv: {}", vdda_mv);
+    Timer::after(Duration::from_millis(100)).await;
+}
 
     loop {
-        // Read pin
-        let v = adc.read(&mut temperature).await;
-        info!("PC1: {}", v);
+        // Read pins
+        // When loop first starts running, there is a significant drift in Vdda (from high down to just under 3V) so we bracket and average
+        let vrefint_sample1 = adc.read(&mut vrefint).await;
+        let t = adc.read(&mut temperature).await;
+        let vrefint_sample2 = adc.read(&mut vrefint).await;
+        let vrefint_sample = vrefint_sample1/2 + vrefint_sample2/2;
+        
+        debug!("temp sample: {}, vrefint_sample: {}, Degrees C: {}", t, vrefint_sample, convert_to_celcius(t, vrefint_sample));
+        info!("Temperature: {} degrees C", convert_to_celcius(t, vrefint_sample));
 
         Timer::after(Duration::from_millis(100)).await;
     }
